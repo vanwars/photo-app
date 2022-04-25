6787569fd8fb121807dbd9ccb3bda5b27c9b2771
Sarah <vanwars@gmail.com>
2022-04-25 14:31:29 -0500

    Edits from class on Monday (4/25)

--- views/posts.py
+++ views/posts.py
@@ -1,6 +1,6 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Post, db
+from models import Post, db, Following
 from views import get_authorized_user_ids
 
 import json
@@ -13,12 +13,35 @@ class PostListEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
 
-    def get(self):
-        # get posts created by one of these users:
-        # print(get_authorized_user_ids(self.current_user))
-        return Response(json.dumps([]), mimetype="application/json", status=200)
 
-    def post(self):
+    def get(self):  #HTTP GET
+        args = request.args 
+        print(args)
+        # Goal: limit to only user #12 (current_user)'s network
+        #   - oneself
+        #   - ppl #12 are following
+
+        #1. Query the following table to get the user_ids that #12 is following:
+        # user_ids_tuples = (
+        #     db.session
+        #         .query(Following.following_id)
+        #         .filter(Following.user_id == self.current_user.id)
+        #         .order_by(Following.following_id)
+        #         .all()
+        #     )
+        # print(user_ids_tuples)
+        # user_ids = [id for (id,) in user_ids_tuples]
+        # print(user_ids)
+        # user_ids.append(self.current_user.id)
+        user_ids = get_authorized_user_ids(self.current_user)
+
+        limit = args.get('limit') or 10 # 10 is the default
+        #posts = Post.query.limit(limit).all()
+        posts = Post.query.filter(Post.user_id.in_(user_ids)).limit(limit).all()
+        posts_json = [post.to_dict() for post in posts]
+        return Response(json.dumps(posts_json), mimetype="application/json", status=200)
+
+    def post(self):   #HTTP POST
         # create a new post based on the data posted in the body 
         body = request.get_json()
         print(body)  
@@ -44,7 +67,8 @@ class PostDetailEndpoint(Resource):
 
     def get(self, id):
         # get the post based on the id
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        print(id)
+        return Response(json.dumps({"id": id}), mimetype="application/json", status=200)
 
 def initialize_routes(api):
     api.add_resource(
