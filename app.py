2d85f0feab8b16f5b03b3764bd1f70c1aedd3796
Sarah <vanwars@gmail.com>
2022-05-11 13:29:07 -0500

    Just set up my hw04 branch

--- app.py
+++ app.py
@@ -31,13 +31,14 @@ initialize_routes(api)
 # Server-side template for the homepage:
 @app.route('/')
 def home():
-    return '''
-       <p>View <a href="/api">REST API Tester</a>.</p>
-       <p>Feel free to replace this code from HW2</p>
-    '''
+    return render_template(
+        'starter-client.html', 
+        user=app.current_user
+    )
 
 
 @app.route('/api')
+@app.route('/api/')
 def api_docs():
     navigator = ApiNavigator(app.current_user)
     return render_template(
